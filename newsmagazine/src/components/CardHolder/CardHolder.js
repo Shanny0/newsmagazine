22c211c82e61e1f2ecae374fc6cc316c216e0f6e
Shanny <rajazeeshanali201gmail.com>
2021-10-27 11:28:38 +0200

    27/10/2021

--- newsmagazine/src/components/CardHolder/CardHolder.js
+++ newsmagazine/src/components/CardHolder/CardHolder.js
@@ -2,105 +2,18 @@ import React from 'react'
 import "./CardHolder_style.scss"
 
 function CardHolder() {
+
+    let getdata = () =>{
+        fetch("https://newsapi.org/v2/everything?q=Apple&from=2021-10-23&sortBy=popularity&apiKey=602e440998c64b46b70a5c06593ffb8e")
+        .then(data => data.json())
+        .then(dt => console.log(dt))
+    }
+
+   
+
     return (
         <div className="container cont_cdhold">
-
-
-            <div class="row">
-                <div className="col">
-                    <div className="card_holder">
-                        <div className="img"><img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /></div>
-                        <div className="tag">Travel</div>
-                        <div className="details">
-                            <div className="title">
-                                Best Place to live
-                            </div>
-                            <div className="subtitle">
-                                lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="col">
-                    <div className="card_holder">
-                        <div className="img"><img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /></div>
-                        <div className="tag">Travel</div>
-                        <div className="details">
-                            <div className="title">
-                                Best Place to live
-                            </div>
-                            <div className="subtitle">
-                                lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="col">
-                    <div className="card_holder">
-                        <div className="img"><img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /></div>
-                        <div className="tag">Travel</div>
-                        <div className="details">
-                            <div className="title">
-                                Best Place to live
-                            </div>
-                            <div className="subtitle">
-                                lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            
-            <div class="row">
-                <div className="col">
-                    <div className="card_holder">
-                        <div className="img"><img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /></div>
-                        <div className="tag">Travel</div>
-                        <div className="details">
-                            <div className="title">
-                                Best Place to live
-                            </div>
-                            <div className="subtitle">
-                                lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="col">
-                    <div className="card_holder">
-                        <div className="img"><img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /></div>
-                        <div className="tag">Travel</div>
-                        <div className="details">
-                            <div className="title">
-                                Best Place to live
-                            </div>
-                            <div className="subtitle">
-                                lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="col">
-                    <div className="card_holder">
-                        <div className="img"><img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /></div>
-                        <div className="tag">Travel</div>
-                        <div className="details">
-                            <div className="title">
-                                Best Place to live
-                            </div>
-                            <div className="subtitle">
-                                lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+        <button onClick={ getdata}>Get Data</button>
 
             <div class="row">
                 <div className="col">
@@ -119,36 +32,15 @@ function CardHolder() {
 
                 </div>
                 <div className="col">
-                    <div className="card_holder">
-                        <div className="img"><img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /></div>
-                        <div className="tag">Travel</div>
-                        <div className="details">
-                            <div className="title">
-                                Best Place to live
-                            </div>
-                            <div className="subtitle">
-                                lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money
-                            </div>
-                        </div>
-                    </div>
-
+                  
                 </div>
                 <div className="col">
-                    <div className="card_holder">
-                        <div className="img"><img src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /></div>
-                        <div className="tag">Travel</div>
-                        <div className="details">
-                            <div className="title">
-                                Best Place to live
-                            </div>
-                            <div className="subtitle">
-                                lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money lroem travel money
-                            </div>
-                        </div>
-                    </div>
+                   
 
                 </div>
             </div>
+
+           
 
         </div>
     )
